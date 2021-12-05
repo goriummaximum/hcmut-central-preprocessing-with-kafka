90d30827a1445be9f06a2c945cde08184e82227a
thiengo <long.hothiengo@hcmut.edu.vn>
2021-12-06 03:37:37 +0700

    update filtering

--- filtering/Preprocessor.java
+++ filtering/Preprocessor.java
@@ -1,4 +1,4 @@
-package com.github.kafka;
+//package com.github.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
@@ -10,13 +10,13 @@ import java.util.*;
 
 public class Preprocessor {
     public static String generate_data(String type, String value, String status) {
-        return "{\"" + type + "\":" + value + ",\"status\":" + status;
+        return "{\"" + type + "\":" + value + ",\"status\":" + status + "}";
     }
 
     public static void main(String[] args) {
         Properties config = new Properties();
-        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "favorite-colour");
-        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "preprocessor");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "128.199.105.69:9091");
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
@@ -26,17 +26,18 @@ public class Preprocessor {
 
         // Assumption: producer sent key-value pairs
         ArrayList <String> valid_sensor_names = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
-            valid_sensor_names.add("temp_" + Integer.toString(i));
-            valid_sensor_names.add("humid_" + Integer.toString(i));
+        for (int i = 0; i < 10; i++) {
+            valid_sensor_names.add("temperature_" + Integer.toString(i));
+            valid_sensor_names.add("humidity_" + Integer.toString(i));
         }
+        //input.to("preprocessed-temperature");
 
         // Filter erroneous records
         KStream<String,String> valid_records = input
                 // Key and value mustn't be null
-                .filter((k, v) -> k == null || v == null)
+                .filter((k, v) -> (k != null && v != null))
                 // Name of the sensor should be valid
-                .filter((sensor_name, value) -> Arrays.asList(valid_sensor_names).contains(sensor_name))
+                .filter((sensor_name, value) -> valid_sensor_names.contains(sensor_name))
                 // Value should be double-parsable
                 .filter((sensor_name, value) -> {
                     try {
@@ -46,29 +47,37 @@ public class Preprocessor {
                         return false;
                     }
                 });
+        //valid_records.to("preprocessed-temperature");
+
         Random r = new Random();
         Timestamp start = new Timestamp(System.currentTimeMillis());
 
         KStream<String, String> sample_records = valid_records
                 // Random sample, 20% dropout
-                .filter((k, v) -> r.nextDouble() > 0.8)
+                .filter((k, v) -> r.nextDouble() < 0.8 || true)
                 // Synchronous sample
                 .filter((k, v) -> {
                     Timestamp now = new Timestamp(System.currentTimeMillis());
-                    return (now.getTime() - start.getTime()) % 5000 < 100;
+                    return (now.getTime() - start.getTime()) % 3000 < 1000 || true;
                 });
+        //sample_records.to("preprocessed-temperature");
 
-        Map<String, KStream<String, String> > branch = sample_records.split()
+        Map<String, KStream<String, String>> branch = sample_records.split()
                 .branch((String sensor_name, String value) -> sensor_name.contains("temp") && Double.valueOf(value) > 10.0 && Double.valueOf(value) < 60.0, Branched.as("safe-temp"))
                 .branch((String sensor_name, String value) -> sensor_name.contains("humid") && Double.valueOf(value) > 20.0 && Double.valueOf(value) < 70.0, Branched.as("safe-humid"))
                 .branch((String sensor_name, String value) -> sensor_name.contains("temp"), Branched.as("unsafe-temp"))
                 .branch((String sensor_name, String value) -> sensor_name.contains("humid"), Branched.as("unsafe-humid"))
-                .noDefaultBranch();
+                .defaultBranch();
 
-        branch.get("safe-temp").mapValues((value) -> generate_data("temp", value, "safe")).to("preprocessed-temperature");
-        branch.get("safe-humid").mapValues((value) -> generate_data("humid", value, "safe")).to("preprocessed-humidity");
-        branch.get("unsafe-temp").mapValues((value) -> generate_data("temp", value, "unsafe")).to("preprocessed-temperature");
-        branch.get("unsafe-humid").mapValues((value) -> generate_data("humid", value, "unsafe")).to("preprocessed-humidity");
+        try {
+            branch.get("safe-temp").mapValues((value) -> generate_data("temp", value, "safe")).to("preprocessed-temperature");
+            branch.get("safe-humid").mapValues((value) -> generate_data("humid", value, "safe")).to("preprocessed-humidity");
+            branch.get("unsafe-temp").mapValues((value) -> generate_data("temp", value, "unsafe")).to("preprocessed-temperature");
+            branch.get("unsafe-humid").mapValues((value) -> generate_data("humid", value, "unsafe")).to("preprocessed-humidity");
+        } catch (NullPointerException e) {
+            System.out.println(e.toString());
+            System.out.println("haha");
+        }
 
         Topology topology = builder.build();
         KafkaStreams streams = new KafkaStreams(topology, config);
